6cd7533ff884ede4aaa8a9ce53a0cc954d6d77d6
bnewey <bnewey@raineyelectronics.com>
2020-07-15 09:54:41 -0500

    didnt hit save on last thing

--- components/Scheduler/TaskList/TaskListTasks.js
+++ components/Scheduler/TaskList/TaskListTasks.js
@@ -78,13 +78,13 @@ const TaskListTasks = (props) =>{
     //// DRAG N DROP
 
     // a little function to help us with reordering the result
-    const reorder = (list, startIndex, endIndex) => {
-      const result = Array.from(list);
-      const [removed] = result.splice(startIndex, 1);
-      result.splice(endIndex, 0, removed);
+    // const reorder = (list, startIndex, endIndex) => {
+    //   const result = Array.from(list);
+    //   const [removed] = result.splice(startIndex, 1);
+    //   result.splice(endIndex, 0, removed);
 
-      return result;
-    };
+    //   return result;
+    // };
 
     const reorderMultiple = (list, ids, endIndex) =>{
       var result = Array.from(list);
